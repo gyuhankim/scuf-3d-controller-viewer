f0952248810999e107fd8e5c37e9950bcd9f3f3c
Joseph Kim <gyuhankim@gmail.com>
2019-02-08 14:02:27 -0500

    added features & witb sections

--- src/App.js
+++ src/App.js
@@ -4,15 +4,45 @@ import './styles/App.css';
 import logo from './images/1.png';
 
 class App extends Component {
+
   render() {
+
     return (
       <div className="App">
       
         <div className="logo-container">
-          <img className="logo" src={logo} alt="scuf-gaming-logo" />
+          <a href="https://scufgaming.com/"><img className="logo" src={logo} alt="scuf-gaming-logo" /></a>
         </div>
 
         <Viewer />
+
+        <div className="controller-information">
+        
+          <div className="controller-features">
+            <h2 className="features-title">
+              FEATURES:
+            </h2>
+            <ul className="feature-list">
+              <li>Poop</li>
+              <li>Moar poop</li>
+              <li>Pewp</li>
+            </ul>
+          </div>
+
+          <div className="witb-container">
+            <div className="whats-in-the-box">
+              <h2 className="witb-title">
+                WHATS IN THE BOX?
+              </h2>
+              <ul className="witb-list">
+                <li>Gold Embossed "Top 10 Ranked US East Player 2019" Certificate</li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+          </div>
+
+        </div>
 
       </div>
     );
--- src/Viewer.js
+++ src/Viewer.js
@@ -5,12 +5,28 @@ import material from './3dmodel/ds4obj.mtl';
 import './styles/Viewer.css';
 
 class Viewer extends Component {
+  constructor(props) {
+    super(props);
+
+    // just a state demo
+    // wouldn't ever set state values as imports
+    this.state = {
+      model: model,
+      material: material
+    }
+  }
+
+  componentDidMount() {
+    console.log("The Dualshock 4 has been successfully loaded!")
+  }
+
   render() {
+
     return (
       <div className="Viewer">
         <OBJModel
-          src={model}
-          mtl={material}
+          src={this.state.model}
+          mtl={this.state.material}
           width={800}
         />
       </div>
